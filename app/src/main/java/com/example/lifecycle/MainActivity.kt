f24e932a1d67fa68729ca33de531826f7c92267b
Bhargav200004 <Bhargav200004@gmail.com>
2024-02-03 19:06:38 +0530

    LifeCycle learning

--- app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -6,16 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.lifecycle.ui.theme.LifeCycleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            println("OnCreate process start")
             LifeCycleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -26,5 +24,30 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("OnStart process start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("OnResume process start")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("OnPause process start")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("OnStop process start")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("OnDestroy process start")
     }
 }
